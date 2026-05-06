72f38f61ab29a464522904a7cb2235a4c2cdcc9c
Steve Crow <steve@smcrow.com>
2026-05-06 17:04:11 -0400

    configure vitest with top-level test/ directory

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		setupFiles: ['./test/setup.ts'],
+		projects: [
+			{
+				extends: true,
+				test: {
+					name: 'components',
+					include: ['test/components/**/*.test.ts'],
+					environment: 'jsdom'
+				}
+			},
+			{
+				extends: true,
+				test: {
+					name: 'unit',
+					include: ['test/**/*.test.ts'],
+					exclude: ['test/components/**'],
+					environment: 'node'
+				}
+			}
+		]
+	}
+});
