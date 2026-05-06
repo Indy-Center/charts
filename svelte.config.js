784af3bc8688174d7297c3d9a86d67492ed67548
Steve Crow <steve@smcrow.com>
2026-05-06 17:09:59 -0400

    add runtime dependencies and Cloudflare type bindings

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 
 const config = {
 	compilerOptions: {
-		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
-	kit: { adapter: adapter() }
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
